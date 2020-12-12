3a297218a00c2190b0c2cd35f13b83747844a998
exmachiana <72504253+exmachiana@users.noreply.github.com>
2020-12-12 20:02:18 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const axios = require("axios")
+const urls = ["https://chisanto-bot-.glitch.me"]
+setInterval(function() {
+            urls.forEach(url => {
+            axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
+        })
+    }, 60 * 1000);
